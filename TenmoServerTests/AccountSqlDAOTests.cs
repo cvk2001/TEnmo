12880154c29de9a8fbabf1a181085723f5086708
Robert Proffitt <proffitt01@gmail.com>
2021-03-06 12:59:32 -0500

    Integration Testing

--- TenmoServerTests/AccountSqlDAOTests.cs
+++ TenmoServerTests/AccountSqlDAOTests.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TenmoServer.DAO;
+using TenmoServer.Models;
 
 namespace TenmoServerTests
 {
+    [TestClass]
     public class AccountSqlDAOTests : TenmoDAOBaseTest
     {
+        [TestMethod]
+        public void GetAccountByUserHappyPath()
+        {
+            AccountSqlDAO dao = new AccountSqlDAO(connectionString);
+
+            Account account = dao.GetAccount(testId1);
+
+            Assert.IsTrue(account != null);
+        }
     }
 }
--- TenmoServerTests/TransferSqlDAOTests.cs
+++ TenmoServerTests/TransferSqlDAOTests.cs
@@ -1,10 +1,54 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TenmoServer.DAO;
+using TenmoServer.Models;
 
 namespace TenmoServerTests
 {
+    [TestClass]
     public class TransferSqlDAOTests : TenmoDAOBaseTest
     {
+        [TestMethod]
+        public void GetTransfersByIdHappyPath()
+        {
+            TransferSqlDAO dao = new TransferSqlDAO(connectionString);
+
+            List<Transfer> transferList = dao.GetTransfers(testId1);
+
+            Assert.IsTrue(transferList.Count > 0);
+        }
+        [TestMethod]
+        public void GetTransferByUserAndTransferIdHAppyPath()
+        {
+            TransferSqlDAO dao = new TransferSqlDAO(connectionString);
+
+            Transfer transfer = dao.GetTransfer(testId1,testTransId);
+
+            Assert.IsTrue(transfer != null);
+        }
+        [TestMethod]
+        public void SendTransferHappyPath()
+        {
+            // Arrange
+            Transfer transfer= new Transfer();
+            transfer.TransferTypeId = 2;
+            transfer.TransferStatusId = 2;
+            transfer.Account_From = testAcc1Id;
+            transfer.Account_To = testAcc2Id;
+            transfer.Amount = 5.00M;
+
+            int startingTransCount = GetRowCount("transfers");
+
+            TransferSqlDAO dao = new TransferSqlDAO(connectionString);
+            // Act
+            dao.TransferSend(transfer);
+
+            int endingTransCount = GetRowCount("transfers");
+
+            //Assert
+            Assert.AreNotEqual(startingTransCount, endingTransCount);
+        }
     }
 }
